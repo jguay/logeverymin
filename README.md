7ac3c823ff8d6b19e8009ba686dbcf1387412d34
Julien Guay <jul.guay@gmail.com>
2020-12-08 10:02:43 +0000

    adding a logLength setting

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Just a simple docker image which sends a message to stdout every 60 seconds by d
 # Usage
 docker CLI :
 ```
-docker run -e 'frequency=1' julienguay/logeverymin:1.0
-[Sat Dec  5 23:18:26 UTC 2020] 0
-[Sat Dec  5 23:18:27 UTC 2020] 1
-[Sat Dec  5 23:18:28 UTC 2020] 2
+docker run -e 'frequency=1' -e 'logLength=10' julienguay/logeverymin:1.0
+[Sat Dec  5 23:18:26 UTC 2020] 0 test test
+[Sat Dec  5 23:18:27 UTC 2020] 1 test test
+[Sat Dec  5 23:18:28 UTC 2020] 2 test test
 ```
--- script.sh
+++ script.sh
@@ -1,13 +1,32 @@
 #/bin/bash
 
-if [[ -z "$frequency" ]]; then
-	frequency=60
-fi
+function assignSettings () {
+	if [[ -z "$frequency" ]]; then
+		frequency=60
+	fi
 
-i=0
-while true
-do
-	echo "[$(date)] ${i}"
-	let i=i+1
-	sleep $frequency
-done
+	if [[ -z "$logLength" ]]; then
+		logLength=0
+	fi
+}
+
+function createLogMessage () {
+	iterations="$(expr $logLength / 5)"
+	for (( i=1 ; i < iterations ; i++)); do
+		msg="${msg}test "
+	done
+}
+
+function showMessages () {
+	i=0
+	while true
+	do
+		echo "{\"timestamp\":\"$(date)\",\"id\":\"${i}\",\"message\":\"${msg}\" }"
+		let i=i+1
+		sleep $frequency
+	done
+}
+
+assignSettings
+createLogMessage
+showMessages
